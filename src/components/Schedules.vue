bda7873e35a92d4ee48e38d6d2b1dac4b88465a1
DENPHUM\black <niklaz.soulless@gmail.com>
2022-05-16 23:21:21 +0700

    [Update] all design

--- src/components/Schedules.vue
+++ src/components/Schedules.vue
@@ -7,178 +7,216 @@ import Delete from "./buttons/Delete.vue";
 import Navbar from "./buttons/Navbar.vue";
 
 const schedules = ref([]);
+const isModalOn = ref(false);
 const URL = "http://intproj21.sit.kmutt.ac.th/at1/api/event";
 
 // GET
 const getSchedules = async () => {
-  const res = await fetch(URL);
-  if (res.status === 200) {
-    schedules.value = await res.json();
-  } else console.log("error, cannot get data");
+    const res = await fetch(URL);
+    if (res.status === 200) {
+        schedules.value = await res.json();
+    } else console.log("error, cannot get data");
 };
 
 onBeforeMount(async () => {
-  await getSchedules();
+    await getSchedules();
 });
 //DELETE
 const removeSchedules = async (removeContentID) => {
-  if (confirm("Do you really want to delete")) {
-    const res = await fetch(URL + "/" + removeContentID, {
-      method: "DELETE",
-    });
-    if (res.status === 200) {
-      schedules.value = schedules.value.filter(
-        (schedules) => schedules.id !== removeContentID
-      );
-      console.log("deleted successfullly");
-    } else console.log("error, cannot delete");
-  }
+    if (confirm("Do you really want to delete")) {
+        const res = await fetch(URL + "/" + removeContentID, {
+            method: "DELETE",
+        });
+        if (res.status === 200) {
+            schedules.value = schedules.value.filter(
+                (schedules) => schedules.id !== removeContentID
+            );
+            console.log("deleted successfullly");
+        } else console.log("error, cannot delete");
+    }
 };
 
 // POST
-const createNewSchedules = async (Name, Email, selectedId, Time, Duration, Notes
+const createNewSchedules = async (
+    Name,
+    Email,
+    selectedId,
+    Time,
+    Duration,
+    Notes
 ) => {
-  const res = await fetch(URL, {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify({
-      bookingName: Name,
-      bookingEmail: Email,
-      categoryId: selectedId,
-      eventStartTime: Time,
-      eventDuration: Duration,
-      eventNotes: Notes,
-    }),
-  });
-  if (res.status === 200) {
-    getSchedules();
-  } else console.log("error, cannot be added");
+    const res = await fetch(URL, {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+        },
+        body: JSON.stringify({
+            bookingName: Name,
+            bookingEmail: Email,
+            categoryId: selectedId,
+            eventStartTime: Time,
+            eventDuration: Duration,
+            eventNotes: Notes,
+        }),
+    });
+    if (res.status === 200) {
+        getSchedules();
+    } else console.log("error, cannot be added");
 };
 
 // EDIT
 const modifySchedules = async (newid, newtime, newnotes) => {
-  const res = await fetch(URL + "/" + newid, {
-    method: "PUT",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify({
-      eventStartTime: newtime,
-      eventNotes: newnotes,
-    }),
-  });
-
-  if (res.status === 200) {
-    getSchedules();
-    const modify = await res.json();
-    schedules.value = schedules.value.map((schedules) =>
-      schedules.id === modify.id
-        ? {
-            ...schedules,
+    const res = await fetch(URL + "/" + newid, {
+        method: "PUT",
+        headers: {
+            "content-type": "application/json",
+        },
+        body: JSON.stringify({
             eventStartTime: newtime,
-            eventNotes: newnotes
-          }
-        : schedules
-    );
+            eventNotes: newnotes,
+        }),
+    });
 
-    console.log("edited successfully");
-  } else console.log("error, cannot edit");
+    if (res.status === 200) {
+        getSchedules();
+        const modify = await res.json();
+        schedules.value = schedules.value.map((schedules) =>
+            schedules.id === modify.id
+                ? {
+                      ...schedules,
+                      eventStartTime: newtime,
+                      eventNotes: newnotes,
+                  }
+                : schedules
+        );
+
+        console.log("edited successfully");
+    } else console.log("error, cannot edit");
 };
 
 const currentDetail = ref({});
 const moreDetail = (curbookingId) => {
-  currentDetail.value = curbookingId;
+    currentDetail.value = curbookingId;
 };
 </script>
 
 <template>
-  <div id="contents-list" v-cloak class="pt-20">
-    <h1 class="text-5xl font-medium py-5">Schedules Event</h1>
-    <div class="overflow-y-scroll table-content">
-      <table class="table table-zebra w-full rounded-full">
-        <thead>
-          <tr>
-            <!-- Narbar -->
-            <Navbar />
-            <th>
-              <!-- Create -->
-              <Create @create="createNewSchedules" />
-            </th>
-          </tr>
-          <!-- Detail  -->
-        </thead>
-        <div v-if="schedules < 1" class="text-5xl pt-20" v-cloak>
-          No Scheduled Events
+    <div id="contents-list" v-cloak class="p-20">
+        <h1 class="text-5xl font-medium py-5">Schedules Event</h1>
+            <table class="table-zebra table-layout table-element">
+                <thead class="table-header bg-base-200">
+                    <tr>
+                        <Navbar/>
+                        <th>
+                            <Create @create="createNewSchedules" />
+                        </th>
+                    </tr>
+                </thead>
+                <div v-if="schedules < 1" class="text-5xl pt-20" v-cloak>
+                    No Scheduled Events
+                </div>
+                <tbody v-else>
+                    <tr v-for="contents in schedules" :key="contents.id">
+                        <td class="p-10 text-xl"><div class="box-element break-words">{{ contents.bookingName }}</div></td>
+                        <td class="p-10 text-xl">
+                            <div class="pt-2">
+                                {{ contents.categoryName }}
+                            </div>
+                        </td>
+
+                        <td class="p-10 text-xl">
+                            {{
+                                moment(contents.eventStartTime).format(
+                                    "D MMMM YYYY, h:mm:ss A"
+                                )
+                            }}
+                        </td>
+
+                        <td class="p-10 text-xl">
+                            {{ contents.eventDuration }} minute
+                        </td>
+
+                        <td>
+                            <div id="showDetail">
+                                <Detail
+                                    @moreDetail="moreDetail(contents)"
+                                    :detail="currentDetail"
+                                    @editDetail="modifySchedules"
+                                />
+
+                                <Delete
+                                    @delete="removeSchedules(contents.id)"
+                                />
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div>
         </div>
-        <tbody v-else>
-          <tr v-for="contents in schedules" :key="contents.id">
-            <td class="p-10 text-xl">{{ contents.bookingName }}</td>
-
-            <td class="p-10 text-xl">
-              {{ contents.categoryName }}
-            </td>
-
-            <td class="p-10 text-xl">
-              {{
-                moment(contents.eventStartTime).format("D MMMM YYYY, h:mm:ss A")
-              }}
-            </td>
-
-            <td class="p-10 text-xl">{{ contents.eventDuration }} minute</td>
-
-            <td>
-              <!-- Delete -->
-              <div id="showDetail">
-                <Delete @delete="removeSchedules(contents.id)" />
-                <!-- MoreDetails -->
-                <Detail
-                  @moreDetail="moreDetail(contents)"
-                  :detail="currentDetail"
-                  @editDetail="modifySchedules"
-                />
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
+        <button @click="isModalOn = !isModalOn" class="btn">dsdsds</button>
+            <div v-show="isModalOn" class="modal">
+                <div class="modal-content">fcdfd</div>
+                <p>tong</p>
+            </div>
+        </div>
 </template>
 
 <style scoped>
 [v-cloak] {
-  display: none;
+    display: none;
 }
-.box-element {
-  @apply my-3;
-  width: 1000px;
-  height: 10px;
-  background-color: #b3853d;
-}
+
 .textarea {
-  @apply textarea-ghost focus:outline-none;
+    @apply textarea-ghost focus:outline-none;
 }
-.table-content {
-  height: 600px;
+
+table {
+    text-align: left;
+    position: relative;
+    border-collapse: collapse;
+    border-radius:6px;
 }
-table th,
-#Name {
-  position: -webkit-sticky;
-  position: sticky;
-  top: 0;
-  z-index: 1;
-}
+
 input,
 textarea {
-  color: rgb(0 0 0);
+    color: rgb(0 0 0);
 }
-#size {
-  font-size: 1rem;
+
+.table-header {
+    position: sticky;
+    top: 0;
+    z-index: 9999;
+    height: 100px;
 }
-.center {
-  margin-left: auto;
-  margin-right: auto;
+.table-layout {
+  table-layout: fixed;
+  width: 100%;  
+}
+.box-element {
+    width:250px;
+}
+.table-element {
+  height: 100px;
+}
+.modal-content {
+    background-color: #ffffff;
+    margin: auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 80%;
+    height: 300px;
+}
+.modal {
+    position: fixed;
+    z-index: 1;
+    padding-top: 300px;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 600px;
+    overflow: auto;
+    background-color: rgb(0, 0, 0);
+    background-color: rgba(0, 0, 0, 0.4);
 }
 </style>
