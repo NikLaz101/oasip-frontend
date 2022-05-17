5756fafe22b64c990d87c263832c2f783b23fb86
hirun <hirun.2542@mail.kmutt.ac.th>
2022-05-17 12:07:26 +0700

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {
     createRouter,
     createWebHistory
@@ -23,30 +22,4 @@ const router = createRouter({
     routes
 });
 
-=======
-import {
-    createRouter,
-    createWebHistory
-} from "vue-router";
-import Homepage from "../views/Homepage.vue";
-import Contents from "../views/Contents.vue";
-
-const history = createWebHistory();
-const routes = [{
-    path: "/",
-    name: "homepage",
-    component: Homepage,
-    children: [{
-        path: "",
-        name: "contents",
-        component: Contents,
-    }, ],
-}, ];
-
-const router = createRouter({
-    history,
-    routes
-});
-
->>>>>>> f190333ae447eda6ea1cf035446f031b564b6b77
 export default router;
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     base: "/at1/",
     plugins: [vue()]
-=======
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-    plugins: [vue()]
->>>>>>> f190333ae447eda6ea1cf035446f031b564b6b77
 })
