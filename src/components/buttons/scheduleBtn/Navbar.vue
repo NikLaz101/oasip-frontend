3b8d5436b6918bbccab5569f38d5ac05682ca67b
NikLaz101 <68820240+NikLaz101@users.noreply.github.com>
2022-05-23 22:01:37 +0700

    Update Navbar.vue

--- src/components/buttons/scheduleBtn/Navbar.vue
+++ src/components/buttons/scheduleBtn/Navbar.vue
@@ -2,25 +2,21 @@
 import { onBeforeMount, ref } from "vue";
 import { useRouter } from "vue-router";
 defineEmits(["option",'upcoming','past']);
-
 const category = ref([]);
 const appRouter = useRouter();
-
+const URL_CATEGORY = "http://intproj21.sit.kmutt.ac.th/at1/api/category";
 // GET
 const getCategories = async () => {
-    const res = await fetch(import.meta.env.VITE_CATEGORY_URL);
+    const res = await fetch(URL_CATEGORY);
     if (res.status === 200) {
         category.value = await res.json();
     } else console.log("error, cannot get data");
 };
-
 onBeforeMount(async () => {
     await getCategories();
 });
-
 const scheduleRouter = () => appRouter.push({ name: "scheduleContents",});
 const categoryRouter = () => appRouter.push({ name: "categoryContents",});
-
 </script>
 
 <template>
