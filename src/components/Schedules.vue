f27c2cb6826a8585086b67015b912c5cee7c18f8
DENPHUM\black <niklaz.soulless@gmail.com>
2022-05-27 19:35:21 +0700

    [Update] upcomming & past

--- src/components/Schedules.vue
+++ src/components/Schedules.vue
@@ -9,305 +9,324 @@ import SortDate from "./buttons/scheduleBtn/SortDate.vue";
 
 const schedules = ref([]);
 
-// GET  
+// GET
 const getSchedules = async () => {
-  const res = await fetch(import.meta.env.VITE_EVENT_URL);
-  if (res.status === 200) {
-    schedules.value = await res.json();
-  } else console.log("error, cannot get data");
+    const res = await fetch(import.meta.env.VITE_EVENT_URL);
+    if (res.status === 200) {
+        schedules.value = await res.json();
+    } else console.log("error, cannot get data");
 };
 
 onBeforeMount(async () => {
-  await getSchedules();
+    await getSchedules();
 });
 //DELETE
 const removeSchedules = async (removeContentID) => {
-  if (confirm("Do you really want to delete")) {
-    const res = await fetch(
-      import.meta.env.VITE_EVENT_URL + "/" + removeContentID,
-      {
-        method: "DELETE",
-      }
-    );
-    if (res.status === 200) {
-      schedules.value = schedules.value.filter(
-        (schedules) => schedules.id !== removeContentID
-      );
-      console.log("deleted successfullly");
-    } else console.log("error, cannot delete");
-  }
+    if (confirm("Do you really want to delete")) {
+        const res = await fetch(
+            import.meta.env.VITE_EVENT_URL + "/" + removeContentID,
+            {
+                method: "DELETE",
+            }
+        );
+        if (res.status === 200) {
+            schedules.value = schedules.value.filter(
+                (schedules) => schedules.id !== removeContentID
+            );
+            console.log("deleted successfullly");
+        } else console.log("error, cannot delete");
+    }
 };
 
 // PUT
 const modifySchedules = async (newId, newTime, newNotes, isOverlap) => {
-  console.log(isOverlap);
-  if (isOverlap) {
-  } else {
-    const res = await fetch(import.meta.env.VITE_EVENT_URL + "/" + newId, {
-      method: "PUT",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({
-        eventStartTime: moment(newTime).utcOffset("+07:00"),
-        eventNotes: newNotes == null ? null : newNotes.trim(),
-      }),
-    });
-    if (res.status === 200) {
-      const edit = await res.json();
-      data.value = edit.eventNotes;
-      getSchedules();
-      console.log("edited successfully");
-    } else console.log("error, cannot edit");
-  }
+    console.log(isOverlap);
+    if (isOverlap) {
+    } else {
+        const res = await fetch(import.meta.env.VITE_EVENT_URL + "/" + newId, {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({
+                eventStartTime: moment(newTime).utcOffset("+07:00"),
+                eventNotes: newNotes == null ? null : newNotes.trim(),
+            }),
+        });
+        if (res.status === 200) {
+            const edit = await res.json();
+            data.value = edit.eventNotes;
+            getSchedules();
+            console.log("edited successfully");
+        } else console.log("error, cannot edit");
+    }
 };
 
 // POST
 const createNewSchedules = async (
-  Name,
-  Email,
-  selectedId,
-  Time,
-  Duration,
-  Notes,
-  isOverlap
+    Name,
+    Email,
+    selectedId,
+    Time,
+    Duration,
+    Notes,
+    isOverlap
 ) => {
-  console.log(isOverlap);
-  if (isOverlap) {
-  } else {
-    const res = await fetch(import.meta.env.VITE_EVENT_URL, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({
-        bookingName: Name,
-        bookingEmail: Email,
-        categoryId: selectedId,
-        eventStartTime: moment(Time).utcOffset("+07:00"),
-        eventDuration: Duration,
-        eventNotes: Notes.trim() == "" ? null : Notes.trim(),
-      }),
-    });
-    if (res.status === 201) {
-      getSchedules();
-    } else console.log("error, cannot be added");
-  }
+    console.log(isOverlap);
+    if (isOverlap) {
+    } else {
+        const res = await fetch(import.meta.env.VITE_EVENT_URL, {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({
+                bookingName: Name,
+                bookingEmail: Email,
+                categoryId: selectedId,
+                eventStartTime: moment(Time).utcOffset("+07:00"),
+                eventDuration: Duration,
+                eventNotes: Notes.trim() == "" ? null : Notes.trim(),
+            }),
+        });
+        if (res.status === 201) {
+            getSchedules();
+        } else console.log("error, cannot be added");
+    }
 };
 
 const currentDetail = ref({});
 const data = ref("");
 
 const moreDetail = (curbookingId) => {
-  currentDetail.value = curbookingId;
-  data.value = curbookingId.eventNotes;
-  currentDetail.value.eventStartTime = moment(
-    currentDetail.value.eventStartTime
-  ).format("YYYY-MM-DDTHH:mm:ss");
-  getSchedules();
+    currentDetail.value = curbookingId;
+    data.value = curbookingId.eventNotes;
+    currentDetail.value.eventStartTime = moment(
+        currentDetail.value.eventStartTime
+    ).format("YYYY-MM-DDTHH:mm:ss");
+    getSchedules();
 };
 
 const filter = ref();
+const upcomingEvent = ref();
+const pastEvent = ref();
 const getClinic = async (id) => {
-  if (id !== 0) {
-    const res = await fetch(
-      import.meta.env.VITE_CATEGORY_URL + "/" + id + "/events"
-    );
-    if (res.status === 200) {
-      filter.value = await res.json();
-      console.log(filter.value);
-    } else console.log("error, cannot get data");
-  } else {
-    filter.value = undefined;
-  }
+    if (id !== 0) {
+        const res = await fetch(
+            import.meta.env.VITE_CATEGORY_URL + "/" + id + "/events"
+        );
+        if (res.status === 200) {
+            filter.value = await res.json();
+            console.log(filter.value);
+        } else console.log("error, cannot get data");
+    } else {
+        filter.value = undefined;
+    }
 };
 
 const getUpcoming = async () => {
-  const res = await fetch(import.meta.env.VITE_EVENT_URL + "/upcoming");
-  if (res.status === 200) {
-    filter.value = await res.json();
-    console.log(filter.value);
-  } else console.log("error, cannot get data");
+    const res = await fetch(import.meta.env.VITE_EVENT_URL + "/upcoming");
+    if (res.status === 200) {
+        upcomingEvent .value = await res.json();
+        console.log(upcomingEvent.value);
+    } else console.log("error, cannot get data");
 };
 
 const getPast = async () => {
-  const res = await fetch(import.meta.env.VITE_EVENT_URL + "/past");
-  if (res.status === 200) {
-    filter.value = await res.json();
-    console.log(filter.value);
-  } else console.log("error, cannot get data");
+    const res = await fetch(import.meta.env.VITE_EVENT_URL + "/past");
+    if (res.status === 200) {
+        pastEvent.value = await res.json();
+        console.log(pastEvent.value);
+    } else console.log("error, cannot get data");
 };
 
 //GET
-const getSortDate = async(date) => {
-    const res = await fetch(import.meta.env.VITE_EVENT_URL + '/sort-date/' + date );
-  if (res.status === 200) {
-    filter.value = await res.json();
-  } else console.log("error, cannot get data");
-}
+const getSortDate = async (date) => {
+    const res = await fetch(
+        import.meta.env.VITE_EVENT_URL + "/sort-date/" + date
+    );
+    if (res.status === 200) {
+        filter.value = await res.json();
+    } else console.log("error, cannot get data");
+};
 </script>
 
 <template>
-  <SortDate @sort-date="getSortDate" />
-  <div id="contents-list" v-cloak class="px-10 py-5 flex justify-center">
-    <table class="table-zebra table-layout table-element">
-      <thead class="table-header bg-base-200">
-        <tr>
-          <Navbar @option="getClinic" @upcoming="getUpcoming" @past="getPast" />
-          <th>
-            <Create :detail="schedules" @create="createNewSchedules" />
-          </th>
-        </tr>
-      </thead>
-      <div
-        id="Noevent"
-        v-if="(schedules< 1) || filter < 1 "
-        class="text-5xl pt-20"
-        v-cloak
-      >
-        No Scheduled Events
-      </div>
-      <tbody v-else>
-        <tr
-          v-if="filter == undefined"
-          v-for="contents in schedules"
-          :key="contents.id"
-        >
-          <td class="p-10 text-xl">
-            <div class="box-element break-words">
-              {{ contents.bookingName }}
+    <SortDate @sort-date="getSortDate" />
+    <div id="contents-list" v-cloak class="px-10 py-5 flex justify-center">
+        <table class="table-zebra table-layout table-element">
+            <thead class="table-header bg-base-200">
+                <tr>
+                    <Navbar
+                        @option="getClinic"
+                        @upcoming="getUpcoming"
+                        @past="getPast"
+                    />
+                    <th>
+                        <Create
+                            :detail="schedules"
+                            @create="createNewSchedules"
+                        />
+                    </th>
+                </tr>
+            </thead>
+            <div
+                v-if="schedules < 1 || filter < 1"
+                class="no-event text-5xl pt-20"
+                v-cloak
+            >
+                No Scheduled Events
             </div>
-          </td>
-          <td class="p-10 text-xl">
-            <div class="pt-2">
-              {{ contents.categoryName }}
+            <div
+                v-else-if="upcomingEvent < 1 || pastEvent < 1"
+                class="no-event text-5xl pt-20"
+                v-cloak
+            >
+                <p v-if="upcomingEvent < 1">No On-Going or Upcoming Events</p>
+                <p v-else>No Past Events</p>
             </div>
-          </td>
+            <tbody v-else>
+                <tr
+                    v-if="filter == undefined"
+                    v-for="contents in schedules"
+                    :key="contents.id"
+                >
+                    <td class="p-10 text-xl">
+                        <div class="box-element break-words">
+                            {{ contents.bookingName }}
+                        </div>
+                    </td>
+                    <td class="p-10 text-xl">
+                        <div class="pt-2">
+                            {{ contents.categoryName }}
+                        </div>
+                    </td>
 
-          <td class="p-10 text-xl">
-            {{
-              moment(contents.eventStartTime)
-                .local()
-                .format("D MMMM YYYY, h:mm:ss A")
-            }}
-          </td>
+                    <td class="p-10 text-xl">
+                        {{
+                            moment(contents.eventStartTime)
+                                .local()
+                                .format("D MMMM YYYY, h:mm:ss A")
+                        }}
+                    </td>
 
-          <td class="p-10 text-xl">{{ contents.eventDuration }} minute</td>
+                    <td class="p-10 text-xl">
+                        {{ contents.eventDuration }} minute
+                    </td>
 
-          <td>
-            <div id="showDetail">
-              <Detail
-                @moreDetail="moreDetail(contents)"
-                :detail="currentDetail"
-                :data="data"
-                :event="schedules"
-                @editDetail="modifySchedules"
-              />
+                    <td>
+                        <div id="showDetail">
+                            <Detail
+                                @moreDetail="moreDetail(contents)"
+                                :detail="currentDetail"
+                                :data="data"
+                                :event="schedules"
+                                @editDetail="modifySchedules"
+                            />
 
-              <Delete @delete="removeSchedules(contents.id)" />
-            </div>
-          </td>
-        </tr>
-        <tr
-          v-else
-          v-for="contents in filter"
-        >
-          <td class="p-10 text-xl">
-            <div class="box-element break-words">
-              {{ contents.bookingName }}
-            </div>
-          </td>
-          <td class="p-10 text-xl">
-            <div class="pt-2">
-              {{ contents.categoryName }}
-            </div>
-          </td>
+                            <Delete @delete="removeSchedules(contents.id)" />
+                        </div>
+                    </td>
+                </tr>
+                <tr v-else v-for="contents in filter">
+                    <td class="p-10 text-xl">
+                        <div class="box-element break-words">
+                            {{ contents.bookingName }}
+                        </div>
+                    </td>
+                    <td class="p-10 text-xl">
+                        <div class="pt-2">
+                            {{ contents.categoryName }}
+                        </div>
+                    </td>
 
-          <td class="p-10 text-xl">
-            {{
-              moment(contents.eventStartTime)
-                .local()
-                .format("D MMMM YYYY, h:mm:ss A")
-            }}
-          </td>
+                    <td class="p-10 text-xl">
+                        {{
+                            moment(contents.eventStartTime)
+                                .local()
+                                .format("D MMMM YYYY, h:mm:ss A")
+                        }}
+                    </td>
 
-          <td class="p-10 text-xl">{{ contents.eventDuration }} minute</td>
+                    <td class="p-10 text-xl">
+                        {{ contents.eventDuration }} minute
+                    </td>
 
-          <td>
-            <div id="showDetail">
-              <Detail
-                @moreDetail="moreDetail(contents)"
-                :detail="currentDetail"
-                :data="data"
-                @editDetail="modifySchedules"
-              />
+                    <td>
+                        <div id="showDetail">
+                            <Detail
+                                @moreDetail="moreDetail(contents)"
+                                :detail="currentDetail"
+                                :data="data"
+                                @editDetail="modifySchedules"
+                            />
 
-              <Delete @delete="removeSchedules(contents.id)" />
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+                            <Delete @delete="removeSchedules(contents.id)" />
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <style scoped>
 [v-cloak] {
-  display: none;
+    display: none;
 }
 
-#Noevent {
-  text-align: center;
-  width: 100%;
-  position: absolute;
-  z-index: -1;
+.no-event {
+    text-align: center;
+    width: 100%;
+    position: absolute;
+    z-index: -1;
 }
 
 table {
-  text-align: left;
-  position: relative;
-  border-collapse: collapse;
-  border-radius: 6px;
+    text-align: left;
+    position: relative;
+    border-collapse: collapse;
+    border-radius: 6px;
 }
 
 input,
 textarea {
-  color: rgb(0 0 0);
+    color: rgb(0 0 0);
 }
 
 .table-header {
-  position: sticky;
-  top: 0;
-  height: 100px;
+    position: sticky;
+    top: 0;
+    height: 100px;
 }
 .table-layout {
-  table-layout: fixed;
-  width: 90%;
+    table-layout: fixed;
+    width: 90%;
 }
 .box-element {
-  width: 250px;
+    width: 250px;
 }
 .table-element {
-  height: 100px;
+    height: 100px;
 }
 .modal-content {
-  background-color: #ffffff;
-  margin: auto;
-  padding: 20px;
-  border: 1px solid #888;
-  width: 80%;
-  height: 300px;
+    background-color: #ffffff;
+    margin: auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 80%;
+    height: 300px;
 }
 .modal {
-  position: fixed;
-  z-index: 1;
-  padding-top: 300px;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 600px;
-  overflow: auto;
-  background-color: rgb(0, 0, 0);
-  background-color: rgba(0, 0, 0, 0.4);
+    position: fixed;
+    z-index: 1;
+    padding-top: 300px;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 600px;
+    overflow: auto;
+    background-color: rgb(0, 0, 0);
+    background-color: rgba(0, 0, 0, 0.4);
 }
 </style>
